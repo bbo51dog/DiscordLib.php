e6839e499902d806bf61450573f6fff22cf4735c
bbo51dog <bbo51dog@gmail.com>
2021-03-23 23:45:03 +0900

    Fix WebSocket

--- src/bbo51dog/discordlib/websocket/WebSocket.php
+++ src/bbo51dog/discordlib/websocket/WebSocket.php
@@ -7,6 +7,12 @@ class WebSocket {
     /** @var string */
     private $host;
 
+    /** @var int */
+    private $port;
+
+    /** @var string */
+    private $path;
+
     /** @var resource */
     private $resource;
 
@@ -14,9 +20,13 @@ class WebSocket {
      * WebSocket constructor.
      *
      * @param string $host
+     * @param int $port
+     * @param string $path
      */
-    public function __construct(string $host) {
+    public function __construct(string $host, int $port = 443, string $path = "/") {
         $this->host = $host;
+        $this->port = $port;
+        $this->path = $path;
     }
 
     /**
@@ -24,24 +34,26 @@ class WebSocket {
      */
     public function open() {
         $key = base64_encode(random_bytes(16));
-        $address = "wss://" . $this->host . ":443";
-        $resource = stream_socket_client($address, $error_code, $error_message);
+        $host = $this->port === 443 ? "ssl://" . $this->host : $this->host;
+        $resource = fsockopen($host, $this->port, $error_code, $error_message);
         if (!$resource) {
             throw new WebSocketException("WebSocket connection failed ({$error_code} {$error_message})");
         }
         $header =
-            "GET / HTTP/1.1" .
-            "Host: {$this->host}" .
-            "Upgrade: websocket" .
-            "Connection: upgrade" .
-            "Sec-WebSocket-Version: 13" .
-            "Sec-WebSocket-Key: {$key}";
+            "GET {$this->path} HTTP/1.1\n" .
+            "Host: {$this->host}\n" .
+            "Upgrade: websocket\n" .
+            "Connection: Upgrade\n" .
+            "Sec-WebSocket-Version: 13\n" .
+            "Sec-WebSocket-Key: {$key}\n\n";
         if (!fwrite($resource, $header)) {
             throw new WebSocketException("Sending header failed");
         }
+        stream_set_timeout($resource, 5);
         $server_response = fread($resource, 1024);
+        $secWebsocketAccept = base64_encode(sha1($key . "258EAFA5-E914-47DA-95CA-C5AB0DC85B11", true));
         if (stripos($server_response, "101") === false ||
-            stripos($server_response, "Sec-WebSocket-Accept: {$key}") === false) {
+            stripos($server_response, "Sec-WebSocket-Accept: {$secWebsocketAccept}") === false) {
             throw new WebSocketException("WebSocket response failed");
         }
         $this->resource = $resource;
